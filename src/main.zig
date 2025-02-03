f5d064b9f57a5c1d0be53efbab50fb89166422cc
simon <arapoglu.simon@gmx.de>
2025-02-03 03:39:04 +0100

    create window

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,14 @@
 const std = @import("std");
+const rl = @import("raylib");
 pub fn main() void {
-    std.debug.print("Hello World", .{});
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "FABRIK");
+    defer rl.closeWindow();
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+        rl.clearBackground(rl.Color.dark_gray); // Set background color (framebuffer clear color)
+    }
 }
