a33e471221190c031c2837399b1d0a844de635d3
simon <you@example.com>
2025-02-07 20:23:06 +0100

    finished basic algo

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,7 @@ pub fn main() !void {
     defer joints.deinit();
 
     for (0..5) |i| {
-        const joint = Joint{ .position = rl.Vector2{ .x = @floatFromInt(screenWidth / 2 + i * 20), .y = @floatFromInt(screenHeight / 2) } };
+        const joint = Joint{ .position = rl.Vector2{ .x = @floatFromInt(screenWidth / 2 + i * 60), .y = @floatFromInt(screenHeight / 2) } };
         try joints.append(joint);
     }
 
@@ -28,8 +28,8 @@ pub fn main() !void {
         rl.clearBackground(rl.Color.dark_gray); // Set background color (framebuffer clear color)
 
         const dists = try computeJointDistances(alloc, joints);
+
         const target = rl.getMousePosition();
-        std.debug.print("{any}\n", .{target});
         stepJoints(joints, dists, target);
         drawJoints(joints);
     }
@@ -39,9 +39,9 @@ fn computeJointDistances(alloc: std.mem.Allocator, joints: ArrayList(Joint)) !Ar
     var dists = ArrayList(f32).init(alloc);
     errdefer dists.deinit();
 
-    for (1..joints.items.len) |index| {
-        const a = joints.items[index - 1];
-        const b = joints.items[index];
+    for (0..joints.items.len - 1) |index| {
+        const a = joints.items[index];
+        const b = joints.items[index + 1];
 
         try dists.append(a.position.distance(b.position));
     }
@@ -50,33 +50,56 @@ fn computeJointDistances(alloc: std.mem.Allocator, joints: ArrayList(Joint)) !Ar
 }
 
 fn stepJoints(joints: ArrayList(Joint), dists: ArrayList(f32), target: rl.Vector2) void {
-    const dTarget = joints.items[0].position.distance(target);
+    const tolerance = 0.001;
+
+    const distTarget = joints.items[0].position.distance(target);
     var allJointDistanceLength: f32 = 0;
     for (dists.items) |value| {
         allJointDistanceLength += value;
     }
 
-    if (dTarget > allJointDistanceLength) {
+    if (distTarget > allJointDistanceLength) {
         for (joints.items[0 .. joints.items.len - 1], 0..) |*joint, i| {
-            const r = joint.position.distance(target);
-            const lambda = dists.items[i] / r;
-            const oldPos = joints.items[i + 1].position;
-            joints.items[i + 1].position = oldPos
-                .multiply(rl.Vector2{ .x = 1.0 - lambda, .y = 1.0 - lambda })
-                .add(target.multiply(rl.Vector2{ .x = lambda, .y = lambda }));
+            const r: f32 = joint.position.distance(target);
+            const lambda: f32 = dists.items[i] / r;
+            joints.items[i + 1].position = joints.items[i].position
+                .scale(1.0 - lambda)
+                .add(target.scale(lambda));
         }
-    } 
-    else {
+    } else {
         const b = joints.items[0].position;
-        const difA = joints.items[joints.items.len - 1].position.distance(target);
-        var i = joints.items.len - 2;
-        while(i >= 0){
-            const pi = joints.items[i];
-            const pi1 = joints.items[i+1];
+        var difA = joints.items[joints.items.len - 1].position.distance(target);
+        while (difA > tolerance) {
 
-            const r = pi.position.distance(pi1.position);
+            // STAGE 1: FORWARD REACHING
+            joints.items[joints.items.len - 1].position = target;
+            var j = joints.items.len - 2;
+            while (true) {
+                const pi = joints.items[j];
+                const pi1 = joints.items[j + 1];
 
-            i--;
+                const r = pi.position.distance(pi1.position);
+                const lambda = dists.items[j] / r;
+
+                joints.items[j].position = pi1.position
+                    .scale(1 - lambda)
+                    .add(pi.position.scale(lambda));
+
+                if (j == 0) break;
+                j -= 1;
+            }
+            // STAGE 2: BACKWARD REACHING
+            joints.items[0].position = b;
+            for (joints.items[0 .. joints.items.len - 1], 0..) |_, i| {
+                const pi = joints.items[i];
+                const pi1 = joints.items[i + 1];
+                const r = pi.position.distance(pi1.position);
+                const lambda = dists.items[i] / r;
+                joints.items[i + 1].position = pi.position
+                    .scale(1 - lambda)
+                    .add(pi1.position.scale(lambda));
+            }
+            difA = joints.items[joints.items.len - 1].position.distance(target);
         }
     }
 }
